e99ca330c8fc7582931878a5762c77f8214223b7
nathanbegbie <nathanbegbie@gmail.com>
2018-07-18 09:47:32 +0200

    Reformatted file

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ if __name__ == "__main__":
     app.run(debug=DEBUG, host="0.0.0.0")
 
 
-if __name__ != '__main__':
-    gunicorn_logger = logging.getLogger('gunicorn.error')
+if __name__ != "__main__":
+    gunicorn_logger = logging.getLogger("gunicorn.error")
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
